51eb42b1113b10fd8bb12df2334098af758cc003
Owen Hurford <owen@hurfo.com>
2016-02-21 19:35:14 +0000

    Basic theme and list on homepage

---
+++ inc/site.js
@@ -0,0 +1,26 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
+    $.ajax({url: '/data/tools.json', success: function (result) {
+        var lists = [].slice.call(window.document.getElementsByClassName('listall'));
+        result = JSON.parse(result);
+        for (var list in lists) {
+            var ul = document.createElement('ul');
+            for (var item in result) {
+                var li = document.createElement('li');
+                var title = document.createTextNode(result[item].title);
+                if (lists[list].getAttribute('data-link') === null || lists[list].getAttribute('data-link') === 'link') {
+                    var a = document.createElement('a');
+                    a.href = result[item].url;
+                    a.title = result[item].title;
+                    a.appendChild(title);
+                    li.appendChild(a);
+                } else {
+                    li.appendChild(title);
+                }
+                ul.appendChild(li);
+            }
+            lists[list].appendChild(ul);
+        }
+    }});
+});
--- index.md
+++ index.md
@@ -5,4 +5,7 @@ permalink: /
 
 DevTools is a guide to the developer tools and resources that I use regularly for different tasks.
 
-It will include a tool chooser in the future.
+It will include a tool chooser and search function in the future.
+
+<section class="listall">
+</section>
